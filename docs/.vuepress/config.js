064e2f242a687da76a0f7a82c8d66b33eeb973c7
tianluhua <tianluhua@hisense.com>
2023-07-18 20:18:28 +0800

    style setting

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ const nav = require("./nav.js");
 module.exports = {
     title: '柯南小海盗的博客',
     description: 'YYY',
+    base: '/knxhd-blogs/',
     dest: 'dist',
     plugins: [
         ["vuepress-plugin-auto-sidebar", {
